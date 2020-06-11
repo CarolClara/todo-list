55226dd08a587499c3cc3452f5c20403ee197a6b
CarolineClara <caroline.clara@sympla.com.br>
2020-06-11 18:46:01 -0300

    Permite a execução de todos os métodos

--- backend/todolistapp/settings.py
+++ backend/todolistapp/settings.py
@@ -10,6 +10,15 @@ ALLOWED_HOSTS = ['*']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
+CORS_ALLOW_METHODS = (
+    'GET',
+    'POST',
+    'PUT',
+    'PATCH',
+    'DELETE',
+    'OPTIONS'
+)
+
 
 # Application definition
 
